ebd5952bbade6a04eaa11228a109928eb9bfb30f
samdav3 <sdavenport@students.mchenry.edu>
2023-02-03 12:14:28 -0600

    inital commit

---
+++ Chapter_1_Hello_World.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+print("Hello world!")
+
+
+let name = "Sam"
+let language = "Swift"
+var message1 = "Welcome to the wonderful world of"
+var message2 = "\(name), Welcome to the wonderful world of \(language)!"
+print(message2)
+print(name, message1, language,"!")
